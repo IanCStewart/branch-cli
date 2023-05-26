3b44b4e8615d1c71f11148d286e42db3dd52182a
IanCStewart <ian.c.stewart@icloud.com>
2023-05-26 22:11:07 +0200

    Created TypeSelector component.

---
+++ src/components/typeSelector.tsx
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import type { FC } from 'react';
+import { Box, Text } from 'ink';
+import { Select } from '@inkjs/ui';
+
+const TYPES = [
+	{
+		label: 'Feature',
+		value: 'feature',
+	},
+	{
+		label: 'Release',
+		value: 'release',
+	},
+	{
+		label: 'Hotfix',
+		value: 'hotfix',
+	},
+	{
+		label: 'Support',
+		value: 'support',
+	},
+];
+
+export const TypeSelector: FC = () => {
+	const [selectedType, setSelectedType] = useState<string | undefined>();
+
+	return (
+		<Box flexDirection="column">
+			{!selectedType && <Select options={TYPES} onChange={setSelectedType} />}
+			{selectedType && (
+				<Text>
+					Branch type: <Text color="green">{selectedType}</Text>
+				</Text>
+			)}
+		</Box>
+	);
+};
